3fb64269297047424f8076a1e4766cc1e8216a09
ricky <rroberson@synapse-games.com>
2020-12-02 00:27:50 -0600

    basic project setup

---
+++ aoc-2020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace aoc2020
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine($"Hello world.");
+		}
+	}
+}
