8a5ae5305e38789bc428758f401b83b2d05068a1
ricky <rroberson@synapse-games.com>
2020-12-03 03:37:25 -0600

    password authentication system

---
+++ aoc-2020/Passcode.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+
+namespace aoc2020
+{
+	public struct Passcode
+	{
+		public readonly int Min;
+		public readonly int Max;
+		public readonly char Char;
+		public readonly string Password;
+
+		public Passcode(int min, int max, char c, string password)
+		{
+			Min = min;
+			Max = max;
+			Char = c;
+			Password = password;
+		}
+
+		public bool IsValid()
+		{
+			var c = Char;
+			var count = Password.Count(x => x == c);
+			return count <= Max && count >= Min;
+		}
+
+		public bool IsValidOTC() => Password[Max - 1] == Char ^ Password[Min - 1] == Char;
+
+		public override string ToString() => $"Range ({Min}, {Max}) {Char} in {Password}";
+	}
+
+	public interface IPasscodeChecker
+	{
+		bool IsValid(Passcode passcode);
+	}
+
+	public struct OTCPasscodeChecker : IPasscodeChecker
+	{
+		public bool IsValid(Passcode passcode)
+			=> passcode.Password[passcode.Max - 1] == passcode.Char
+			^ passcode.Password[passcode.Min - 1] == passcode.Char;
+	}
+
+	public struct SLEDPasscodeChecker : IPasscodeChecker
+	{
+		public bool IsValid(Passcode passcode)
+		{
+			var count = passcode.Password.Count(x => x == passcode.Char);
+			return count <= passcode.Max && count >= passcode.Min;
+		}
+	}
+}
--- aoc-2020/Program.cs
+++ aoc-2020/Program.cs
@@ -1,80 +1,36 @@
 ﻿using System;
 using System.Linq;
 using System.IO;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 
 namespace aoc2020
 {
-	class MainClass
+	partial class MainClass
 	{
-		const int target = 2020;
-
 		public static void Main(string[] args)
 		{
-			var values = File.ReadAllLines(@"Data/input-D01.txt")
-				.Select(line => int.Parse(line))
+			var checker = new OTCPasscodeChecker();
+			Regex expression = new Regex(@"(?<min>\d+)-(?<max>\d+)\s(?<char>\w):\s(?<password>\w+)");
+			var values = File.ReadAllLines(@"Data/input-D02.txt")
+				.Select(line => {
+					var match = expression.Match(line);
+					if (match.Success) {
+						return new Passcode(
+							int.Parse(match.Groups["min"].Value),
+							int.Parse(match.Groups["max"].Value),
+							match.Groups["char"].Value[0],
+							match.Groups["password"].Value
+						);
+					}
+
+					Console.WriteLine($"Failed to parse: {line}");
+					return new Passcode();
+				})
+				.Where(passcode => checker.IsValid(passcode))
 				.ToList();
 
-			//if (FindSums(values, out var x, out var y)) {
-			//	Console.WriteLine($"Found! {values[x]} * {values[y]} = {values[x] * values[y]}");
-			//} else {
-			//	Console.WriteLine($"Sum {target} not found.");
-			//}
-
-			if (FindSums3 (values, out var arr)) {
-				var product = arr[0];
-				for (int i = 1; i < arr.Length; i++) { product *= arr[i]; }
-				Console.WriteLine($"Found! {string.Join (", ", arr)}. Product = {product}");
-			} else {
-				Console.WriteLine($"Sum {target} not found.");
-			}
-		}
-
-		static bool FindSums2 (List<int> list, out int x, out int y)
-		{
-			list.Sort();
-
-			x = 0;
-			y = list.Count() - 1;
-
-			do {
-				while (list[x] + list[y] < target) {
-					x++;
-				}
-
-				if (list[x] + list[y] == target) {
-					return true;
-				}
-
-				while (list[x] + list[y] > target) {
-					y--;
-				}
-			} while (x <= y);
-
-			return false;
-		}
-
-		static bool FindSums3 (List<int> list, out int[] arr)
-		{
-			list.Sort();
-
-			for (int i = 0; i < list.Count; i++) {
-				for (int j = i + 1; j < list.Count; j++) {
-					for (int k = j + 1; k < list.Count; k++) {
-						if (list[i] + list[j] + list[k] == target) {
-							arr = new int[] {
-								list[i],
-								list[j],
-								list[k]
-							};
-							return true;
-						}
-					}
-				}
-			}
-
-			arr = null;
-			return false;
+			Console.WriteLine($"Number of valid passcodes: {values.Count()}");
 		}
 	}
 }
