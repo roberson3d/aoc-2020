59b08d6ba6afed4f7b84f534328124f8b3da2e11
ricky <rroberson@synapse-games.com>
2020-12-03 18:15:46 -0600

    split out into files

--- aoc-2020/Program.cs
+++ aoc-2020/Program.cs
@@ -74,35 +74,5 @@ namespace aoc2020
 			trees *= toboggan.Sled(map, new Slope(1, 2));
 			Console.WriteLine($"pt2 Tree Count: {trees}");
 		}
-
-		public struct Slope {
-			public readonly int right;
-			public readonly int down;
-
-			public Slope(int right, int down)
-			{
-				this.right = right;
-				this.down = down;
-			}
-		}
-
-		public struct Toboggan {
-			public int Sled (in string[] map, Slope slope)
-			{
-				int trees = 0;
-				for (int x = 0, y = 0; y < map.Length; x += slope.right, y += slope.down) {
-					var plot = map[y][x % map[y].Length];
-
-					var isTree = plot == '#';
-					if (isTree) {
-						trees++;
-					}
-
-					//Console.WriteLine($"{map[y]} ::: {(isTree ? trees.ToString() : ".")}   ({x.ToString()}, {y.ToString()})");
-				}
-
-				return trees;
-			}
-		}
 	}
 }
