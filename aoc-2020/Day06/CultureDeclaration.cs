018c53484fae590523c9c0e2451ce50feb243df4
ricky <rroberson@synapse-games.com>
2020-12-06 14:22:16 -0600

    streamlined answer

--- aoc-2020/Day06/CultureDeclaration.cs
+++ aoc-2020/Day06/CultureDeclaration.cs
@@ -5,33 +5,22 @@ namespace aoc2020
 {
 	public struct CultureDeclaration
 	{
-		public readonly string[] answers;
-		public readonly char[] consolidated;
-		public readonly int allYesCount;
-		public readonly int atLeastOneYesCount;
-		public readonly int groupMembers;
+		public readonly int[] tally;
+		public readonly int memberCount;
 
 		public CultureDeclaration(string notes)
 		{
-			answers = notes.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-			consolidated = notes
-				.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
-				.SelectMany(answer => answer.ToCharArray())
-				.Distinct()
-				.OrderBy(c => c)
-				.ToArray();
+			tally = new int[26];
 
-			var unique = new int[26];
 			var a = (int)'a';
 			foreach (var c in notes) {
 				if (char.IsLetter (c))
-					unique[c - a]++;
+					tally[c - a]++;
 			}
 
-			var members = answers.Count();
-			groupMembers = members;
-			allYesCount = unique.Count(entry => entry == members);
-			atLeastOneYesCount = consolidated.Count();
+			memberCount = notes
+				.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+				.Count();
 		}
 	}
 }
--- aoc-2020/Program.cs
+++ aoc-2020/Program.cs
@@ -149,10 +149,11 @@ namespace aoc2020
 			var data = File.ReadAllText(@"Day06/input.txt")
 				.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
 				.Select(val => new CultureDeclaration(val));
-			var pt1 = data.Sum(cd => cd.atLeastOneYesCount);
-			var pt2 = data.Sum(cd => cd.allYesCount);
 
+			var pt1 = data.Sum(cd => cd.tally.Count(entry => entry > 0));
 			Console.WriteLine($"pt1 Sum: {pt1}");
+
+			var pt2 = data.Sum(cd => cd.tally.Count(entry => entry == cd.memberCount));
 			Console.WriteLine($"pt2 Sum: {pt2}");
 
 		}
